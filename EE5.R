c2f7ae1baaf516f28ccbf158c6f983e6e49a358e
jentlapalm <81242269+jentlapalm@users.noreply.github.com>
2021-12-01 15:00:40 -0500

    Add files via upload

--- EE5.R
+++ EE5.R
@@ -37,9 +37,22 @@ codeals <- invdeal[,.N,by=Deal_Id][N>1]
 # deals dataset limited to deals with at least 2 investors
 subdeals <- invdeal[Deal_Id %in% codeals$Deal_Id]
 
+# grab deals where investor is a lead
+j <- '10013-77' # investor with many deals for testing
 
+temp_deals <- subdeals[Investor_Id==j & Lead_Investor==1,Deal_Id]
+subdeals[Deal_Id %in% temp_deals]
 
-# count how many had A as the lead investor
-
+# looking for a result with multiple deals
+for (i in 1:100){
+  temp_deals <- subdeals[Investor_Id==unique(subdeals$Investor_Id)[i] & 
+                           Lead_Investor==1,Deal_Id]
+  if(length(temp_deals)>1){
+    print('investor')
+    print(unique(subdeals$Investor_Id)[i])
+    print('deals')
+    print(temp_deals)
+  }
+}
 
 "Then, each investor’s status can be represented as the Bonacich centrality of this matrix—an investor’s status is represented by its ability to be a leader on deals, as well as to be connected to other firms that lead their own deals. You can calculate Bonacich centrality using power_centrality() with the argument exponent = 0.75 to represent the commonly-used beta parameter. For the analysis, only consider firms that are actually a part of the status hierarchy, i.e., have co-invested with other firms and have non-missing values for status. To allow older ties to weaken over time, you can exclude ties that have not been renewed after five years."
